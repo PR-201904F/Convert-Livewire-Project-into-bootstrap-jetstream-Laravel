94c05727d11e65cdfa84096377ac27f6c19f075a
Ameer Hamza <82038981+PR-201904F@users.noreply.github.com>
2021-04-22 10:25:19 +0500

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+//add this trade
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //add this class
+        Schema::defaultStringLength(191);
+    }
+}
